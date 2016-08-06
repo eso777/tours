11b2034d06491fac818ed0542b276a9c308fa5c9
Ahmed Gad <ahmed.gad@sawa4.com.eg>
2016-08-06 13:20:13 +0300

    hotels reserv

--- app/HotelsReservations.php
+++ app/HotelsReservations.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class HotelsReservations extends Model {
 
-	//
+	protected $table = "hotels_reservations" ;
+ 	protected $fillable = ['hotel_id','user_id','persons','date_from','date_to']; 
 
 }
